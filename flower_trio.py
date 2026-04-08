23595ddbf4ed920144d78e09e2c684ebc3efa345
Michael Reynolds <mar0179@auburn.edu>
2026-04-07 19:41:35 -0500

    I just need to figure out how to get the iris dataframe to be read into the functions of the program. I am almost finished with this exercise!

--- flower_trio.py
+++ flower_trio.py
@@ -8,6 +8,31 @@ import scipy
 if __name__ == '__main__':
     print ('hello!')
 
+def species (csv, category, cat_name):
+    dataframe = pd.read_csv(csv)
+    species = dataframe[csv.category == cat_name]
+    return species
+
+def linear_reg (dataframe, name, color, save_name):
+    x = dataframe.petal_length_cm
+    y = dataframe.sepal_length_cm
+    regression = stats.linregress(x, y)
+    slope = regression.slope
+    intercept = regression.intercept
+    plt.scatter(x, y, label = name)
+    plt.plot(x, slope * x + intercept, color = color, label = name+'fitted line') 
+    plt.xlabel("Petal length (cm)") 
+    plt.ylabel("Sepal length (cm)") 
+    plt.legend()
+    plt.savefig(save_name)
+    plt.show()
+    return
+
+linear_reg(species(iris.csv, 'iris_versicolor', 'species'), 'versicolor', green, "ver_petal_v_sepal_length_regress.png")
+linear_reg(species(iris.csv, 'iris_setosa', 'species'), 'setosa', blue, "set_petal_v_sepal_length_regress.png")
+linear_reg(species(iris.csv, 'iris_virginica', 'species'), 'virginica', purple, "vir_petal_v_sepal_length_regress.png")
+
+
 
 """
 import pandas as pd
@@ -42,13 +67,8 @@ writing your script.
 
 """
 Code Anatomy
-Code that
-
-Code that returns a graph of an entered x,y, slope, intercept, color
 
 Organs:
-
-def :
 
 def species (csv, category, cat_name):
     dataframe = pd.read_csv(csv)
